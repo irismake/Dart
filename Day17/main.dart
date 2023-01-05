5867859bb2809c88bb50f2ccba3380173109353b
Iris <iris3455@gmail.com>
2023-01-05 20:16:07 +0900

    Abstract Class
    Abstract classes force you to implement certain methods

---
+++ Day17/main.dart
@@ -0,0 +1,41 @@
+abstract class Human {
+  void walk();
+}
+
+enum Team { red, blue }
+
+enum XPLevel { beginner, medium, pro }
+
+class Player extends Human {
+  String name;
+  XPLevel xp;
+  Team team;
+
+  Player({
+    required this.name,
+    required this.xp,
+    required this.team,
+  });
+
+  void walk() {
+    print("I'm walking");
+  }
+
+  void sayHello() {
+    print("Hi my name is $name");
+  }
+}
+
+class Coach extends Human {
+  void walk() {
+    print("the coach is walking");
+  }
+}
+
+void main() {
+  var iris = Player(name: "iris", xp: XPLevel.beginner, team: Team.red)
+    ..name = 'nico'
+    ..xp = XPLevel.medium
+    ..team = Team.blue
+    ..sayHello();
+}
