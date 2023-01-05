2fd811541b914931b7a6e392dd5bc12ee55fabd4
Iris <iris3455@gmail.com>
2023-01-05 20:07:32 +0900

    enum
    enum Team { red, blue }

---
+++ Day16/main.dart
@@ -0,0 +1,27 @@
+enum Team { red, blue }
+
+enum XPLevel { beginner, medium, pro }
+
+class Player {
+  String name;
+  XPLevel xp;
+  Team team;
+
+  Player({
+    required this.name,
+    required this.xp,
+    required this.team,
+  });
+
+  void sayHello() {
+    print("Hi my name is $name");
+  }
+}
+
+void main() {
+  var iris = Player(name: "iris", xp: XPLevel.beginner, team: Team.red)
+    ..name = 'nico'
+    ..xp = XPLevel.medium
+    ..team = Team.blue
+    ..sayHello();
+}
