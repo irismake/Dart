30077c04a161e91f6a8ab29a2ca2c3235ffcb595
Iris <iris3455@gmail.com>
2023-01-05 19:06:47 +0900

    method in class
    this.name -> Used only when the names of variable and class properties overlap

--- Day 10/main.dart
+++ Day 10/main.dart
@@ -1,6 +1,13 @@
 class Player {
   String name = 'iris';
   int xp = 1500;
+
+  void sayHello() {
+    var name = '121';
+    print("Hi my name is ${this.name}");
+    // variable 과 class property 의 이름이 겹칠때만 있을때만 사용
+    // do not use this.name
+  }
 }
 
 void main() {
@@ -8,4 +15,6 @@ void main() {
   print(player.name);
   player.name = 'lalaala';
   print(player.name);
+
+  player.sayHello();
 }
